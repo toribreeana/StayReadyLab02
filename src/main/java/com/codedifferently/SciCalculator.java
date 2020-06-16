df6136bd919b40faead9c45fafe129f88b2882df
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2020-06-16 17:23:38 -0400

    First update

---
+++ src/main/java/com/codedifferently/SciCalculator.java
@@ -0,0 +1,13 @@
+package com.codedifferently;
+
+/**
+ * Hello world!
+ *
+ */
+public class SciCalculator 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
---
+++ src/test/java/com/codedifferently/SciCalculatorTest.java
@@ -0,0 +1,17 @@
+package com.codedifferently;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SciCalculatorTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
